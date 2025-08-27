793f8c376f83cf6d12217b4bcd2d43f14c291949
kithenry <21903316@emu.edu.tr>
2025-08-27 17:02:02 +0300

    Stop tracking ignored directories

--- README.md
+++ README.md
@@ -14,13 +14,29 @@
 
 ## Project timeline
 - [x] WEEK 1: Setup Supabase Project and Electron.js Boilerplate
+<<<<<<< HEAD
       - Time span: 28th July to 4th Aug
       - Completed earlier than anticipated.
       - Ahead of schedule (covered more than i planned to). Code still unsafe.
       - Nodeintegration true, contextisolation false
 - [ ] WEEK 2: Implement CRUD Functionality (Add/View/Delete/Update)
+=======
+      Time span: 28th July to 4th Aug
+- [x] WEEK 2: Implement CRUD Functionality (Add/View/Delete/Update)
+>>>>>>> 751a081 (V1 ready for shipping)
       Time span: 5th Aug to 11th Aug
-- [ ] WEEK 3: Improve UI with Bootstrap, Implement Search Feature
+- [x] WEEK 3: Improve UI with Bootstrap, Implement Search Feature
       Time span: 12th Aug to 18th Aug
-- [ ] WEEK 4: Final testing, Github documentation, delivery
+- [x] WEEK 4: Final testing, Github documentation, delivery
       Time span: 19th Aug to 28th Aug
+
+## Available distributables
+- Linux, zip
+
+## Future Improvements
+- Login interface
+- Data validation for vehicle data input
+- Offline data updates with resync on re connection
+
+## Installation
+- npm run make (currently supports only linux)
--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const { createClient } = require("@supabase/supabase-js");
+//const supabase = createClient(
+//  "https://ekzvjknqvimgfzkklcym.supabase.co",
+ // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrenZqa25xdmltZ2Z6a2tsY3ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNDk4MTUsImV4cCI6MjA2OTYyNTgxNX0.F7iFXAnOeioJvUphIf20HyBQQs68OzUW_OXurP3fKFI",
+// );
+
 const supabase = createClient(
-  "https://ekzvjknqvimgfzkklcym.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrenZqa25xdmltZ2Z6a2tsY3ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNDk4MTUsImV4cCI6MjA2OTYyNTgxNX0.F7iFXAnOeioJvUphIf20HyBQQs68OzUW_OXurP3fKFI",
-);
+  "https://ilhvzapgsibeitinyleg.supabase.co",
+  "sb_publishable_EIp2QOYsfOsfCcANWRMFhA_7f-eRWxP"
+)
 
 async function loadVehicles(searchQuery = "") {
   // initial assumption is no searchQuery is provided, so return everything
-  // supabase var is assumed to be a global provided in the html via a cdn ..
-  //  <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
   let query = supabase.from("vehicles").select("*");
   // during filtered fetch, where searchQuery is something other than none,
   // find all brands and or plates that match the searchQuery
@@ -62,12 +65,13 @@ document
       return;
     }
     document.getElementById("vehicle-form").reset();
-    loadVehicles();
+    loadVehicles(); // Display updated vehicle list
   });
 
 async function deleteVehicle(id) {
   // select all entries from vehicle table with id matching id argument value, and delete them
   // return signal to tell if there was any error carrying out delete operation
+  // refresh display to reflect updated tables after delete operation
   const { error } = await supabase.from("vehicles").delete().eq("id", id);
   if (error) {
     console.error(`Error deleting vehicle: `, error);
@@ -106,7 +110,7 @@ async function editVehicle(id) {
     const { error } = await supabase
       .from("vehicles")
       .update(updatedVehicle)
-      .eq("id", id);
+      .eq("id", id); // get an existing row and update its contents with whats in 'UpdatedVehicle'
     if (error) {
       console.error("Error updating vehicle:", error);
       return;
@@ -124,4 +128,5 @@ document.getElementById("search").addEventListener("input", (e) => {
 });
 
 // after the site html is loaded, we load any database data we have to site.
+// the onload call back is set to a function that triggers what updates the ui with available data
 window.onload = () => loadVehicles();
